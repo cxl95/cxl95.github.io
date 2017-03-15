274b34e5df06a257bc94ed3c65f98e6616814baf
ChenXiaolin <cxl95@163.com>
2017-03-15 11:29:58 +0800

    Site updated: 2017-03-15 11:29:58

---
+++ js/header.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+    
+     $('#logoandmore').hover(
+         function () {$('#arrow').css('background-position', 'center 0px') ;}, 
+         function () {$('#arrow').css('background-position', 'center -16px') ;}
+        );
+    
+     $('#logoandmore').click(
+         function (e) {
+            e.preventDefault();
+            $('#menu').slideToggle('1000', "easeOutQuad", function () {
+                
+                if ($(this).is(':visible')) {
+                        $('#header').addClass('active');
+                        $('#header').removeClass('inactive');
+                        $('#arrow').css("background-image", "url(css/img/arrow-sprite-active.png)");} 
+                else {
+                        $('#header').addClass('inactive');
+                        $('#header').removeClass('active');
+                        $('#arrow').css("background-image", "url(css/img/arrow-sprite.png)");}
+            })});
+});
